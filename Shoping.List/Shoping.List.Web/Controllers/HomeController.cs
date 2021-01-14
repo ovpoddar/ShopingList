2a46884e705f4fa52a11ca2ba41d18a23f59e56b
Ayan Poddar <ayanpoddar0@gmail.com>
2021-01-14 22:08:37 +0530

    add the project

---
+++ Shoping.List/Shoping.List.Web/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Shoping.List.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shoping.List.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
